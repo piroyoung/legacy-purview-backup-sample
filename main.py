bd46090bec4a4e5626ddcf2a9b428f482217c10d
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-05-12 06:36:55 +0900

    add: classify

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from datetime import datetime
 
 from azure.identity import ClientSecretCredential
 from azure.purview.datamap import DataMapClient
+
 from pvsnapshot.env import Environments
 from pvsnapshot.repository import LocalSnapshotRepository
 from pvsnapshot.repository import RemoteRepository
 from pvsnapshot.repository import RestRemoteRepository
 from pvsnapshot.repository import SnapshotRepository
-from pvsnapshot.service import DumpService
+from pvsnapshot.service import DumpService, ClassifyDumpService
 
 logging.basicConfig(level=logging.INFO)
 
@@ -26,7 +27,10 @@ if __name__ == "__main__":
     )
 
     remote: RemoteRepository = RestRemoteRepository(c=client)
-    local: SnapshotRepository = LocalSnapshotRepository()
+    local: SnapshotRepository = LocalSnapshotRepository(dir="snapshots")
 
-    service: DumpService = DumpService(remote=remote, local=local, key=datetime.now().strftime("%Y%m%d%H%M%S"))
-    service.run()
+    key: str = datetime.now().strftime("%Y%m%d%H%M%S")
+    dump: DumpService = DumpService(remote=remote, local=local, key=key)
+    classify: ClassifyDumpService = ClassifyDumpService(local=local, key=key)
+    dump.run()
+    classify.run()
--- pvsnapshot/model.py
+++ pvsnapshot/model.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
+from typing import Any, List, Set
 from typing import Dict
 
-__all__ = ["DataCatalog"]
+__all__ = [
+    "DataCatalog",
+    "classify_with_object_type"
+]
 
 
 @dataclass
@@ -11,3 +14,22 @@ class DataCatalog:
     key: str
     created_at: datetime
     data: Dict[str, Any]
+
+
+def classify_with_object_type(data: DataCatalog) -> List[DataCatalog]:
+    object_types: Set = set()
+    for obj in data.data["value"]:
+        if "objectType" in obj:
+            object_types.add(obj["objectType"])
+
+    catalogs: Dict[str, DataCatalog] = {
+        object_type: DataCatalog(key=f"{data.key}_{object_type.replace(" ", "_")}", created_at=data.created_at,
+                                 data={"value": []})
+        for object_type in object_types
+    }
+
+    for obj in data.data["value"]:
+        if "objectType" in obj:
+            catalogs[obj["objectType"]].data["value"].append(obj)
+
+    return list(catalogs.values())
